daf23783314df678f1bb19e7a4cd8af7c32eaf18
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-06-13 19:15:46 -0300

    feat: adjustments on db entity

--- pix/src/main/java/com/open/pix/infra/entity/PixKeyEntity.java
+++ pix/src/main/java/com/open/pix/infra/entity/PixKeyEntity.java
@@ -1,18 +1,19 @@
 package com.open.pix.infra.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Table(name = "pix_key")
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "pix_key")
 public class PixKeyEntity {
 
     @Id
@@ -20,25 +21,38 @@ public class PixKeyEntity {
     @Column(name = "id", nullable = false, columnDefinition = "UUID")
     private UUID id;
 
+    @NotBlank
     @Column(name = "type", length = 9, nullable = false)
+    @Size(max = 9)
     private String pixType;
 
+    @NotBlank
     @Column(name = "value", length = 77, nullable = false)
+    @Size(max = 77)
     private String value;
 
+    @NotBlank
     @Column(name = "account_type", length = 10, nullable = false)
+    @Size(max = 10)
     private String accountType;
 
-    @Column(name = "agency", nullable = false)
+    @Column(name = "agency", nullable = false, columnDefinition = "NUMERIC(4)")
+    @Min(0)
+    @Max(9999)
     private Integer agencyNumber;
 
-    @Column(name = "account_number", nullable = false)
+    @Column(name = "account_number", nullable = false, columnDefinition = "NUMERIC(8)")
+    @Min(0)
+    @Max(99999999)
     private Integer accountNumber;
 
+    @NotBlank
     @Column(name = "first_name", length = 30, nullable = false)
+    @Size(min = 1, max = 30)
     private String firstName;
 
     @Column(name = "last_name", length = 45)
+    @Size(max = 45)
     private String lastName;
 
     @Column(name = "active", nullable = false)
