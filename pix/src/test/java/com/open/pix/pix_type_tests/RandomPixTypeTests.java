ee2d8030eea9a976baa6ae4d6430e623ae6b16b8
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-06-17 20:40:31 -0300

    feat: incremented pix type tests

--- pix/src/test/java/com/open/pix/pix_type_tests/RandomPixTypeTests.java
+++ pix/src/test/java/com/open/pix/pix_type_tests/RandomPixTypeTests.java
@@ -9,38 +9,44 @@ public class RandomPixTypeTests {
 
     @Test
     void mustThrowWhenNull() {
-        Assertions.assertThrows(PixTypeException.class, () -> {
+        PixTypeException exception = Assertions.assertThrows(PixTypeException.class, () -> {
             new RandomPixType(null);
         });
+
+        Assertions.assertEquals("Random key cannot be empty", exception.getMessage());
     }
 
     @Test
     void mustThrowWhenBlank() {
-        Assertions.assertThrows(PixTypeException.class, () -> {
+        PixTypeException exception = Assertions.assertThrows(PixTypeException.class, () -> {
             new RandomPixType("   ");
         });
+
+        Assertions.assertEquals("Random key cannot be empty", exception.getMessage());
     }
 
     @Test
     void mustThrowWhenTooLong() {
         String tooLong = "A".repeat(37);
-        PixTypeException ex = Assertions.assertThrows(PixTypeException.class,
-                () -> new RandomPixType(tooLong));
+        PixTypeException ex = Assertions.assertThrows(PixTypeException.class, () -> new RandomPixType(tooLong));
         Assertions.assertTrue(ex.getMessage().contains("maxim of 36"));
     }
 
     @Test
     void mustThrowWhenContainsNonAlphanumeric() {
-        Assertions.assertThrows(PixTypeException.class, () -> {
+        PixTypeException exception = Assertions.assertThrows(PixTypeException.class, () -> {
             new RandomPixType("abc123!@#");
         });
+
+        Assertions.assertEquals("Random key must contain just alphanumeric characters", exception.getMessage());
     }
 
     @Test
     void mustAcceptExactlyMaxLength() {
-        String maxLen = "A1".repeat(18); // 36 chars
+        String maxLen = "A1".repeat(18);
         Assertions.assertDoesNotThrow(() -> new RandomPixType(maxLen));
-        Assertions.assertEquals(maxLen, new RandomPixType(maxLen).value());
+        RandomPixType pix = new RandomPixType(maxLen);
+        Assertions.assertEquals(maxLen, pix.value());
     }
 
     @Test
@@ -48,5 +54,8 @@ public class RandomPixTypeTests {
         Assertions.assertDoesNotThrow(() -> {
             new RandomPixType("abcXYZ123");
         });
+        RandomPixType pix = new RandomPixType("abcXYZ123");
+
+        Assertions.assertEquals("abcXYZ123", pix.value());
     }
 }
