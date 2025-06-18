6da4feb1248694614e4847142fdd9813daba367b
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-06-18 10:39:52 -0300

    creating redis configuration

---
+++ pix/src/main/java/com/open/pix/infra/configuration/RedisConfig.java
@@ -0,0 +1,30 @@
+package com.open.pix.infra.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String hostName;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    /**
+     * This factory method provides the lettuce connection for redis, witch is
+     * important to standalone connections.
+     * @return an object of type LettuceConnectionFactory.
+     */
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+}
